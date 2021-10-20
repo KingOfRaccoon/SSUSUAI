dfa3269a29c179195965c67c377ded7a5670c44e
kingofraccoons <zvezdakov2015@yandex.ru>
2021-10-20 18:54:29 +0300

    Создан загрузочный экран

--- app/src/main/java/com/castprogramms/ssusuai/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/castprogramms/ssusuai/ui/profile/ProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.castprogramms.ssusuai.ui
+package com.castprogramms.ssusuai.ui.profile
 
 import androidx.fragment.app.Fragment
 import com.castprogramms.ssusuai.R
---
+++ app/src/main/java/com/castprogramms/ssusuai/ui/splash/SplashFragment.kt
@@ -0,0 +1,7 @@
+package com.castprogramms.ssusuai.ui.splash
+
+import androidx.fragment.app.Fragment
+import com.castprogramms.ssusuai.R
+
+class SplashFragment: Fragment(R.layout.fragment_splash) {
+}
