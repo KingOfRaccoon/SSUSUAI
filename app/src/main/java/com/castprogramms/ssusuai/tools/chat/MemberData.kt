5dda596a6d283fd92f84768753d2ae11b4d708bb
kingofraccoons <zvezdakov2015@yandex.ru>
2021-11-01 21:14:22 +0300

    Переделан MemberData.kt в data class

--- app/src/main/java/com/castprogramms/ssusuai/tools/chat/MemberData.kt
+++ app/src/main/java/com/castprogramms/ssusuai/tools/chat/MemberData.kt
@@ -1,15 +1,3 @@
 package com.castprogramms.ssusuai.tools.chat
 
-class MemberData(name: String?, color: String?) {
-    var name: String? = name
-        private set
-    var color: String? = color
-        private set
-
-    override fun toString(): String {
-        return "MemberData{" +
-                "name='" + name + '\'' +
-                ", color='" + color + '\'' +
-                '}'
-    }
-}
+data class MemberData(val name: String = "", val color: String = "")
